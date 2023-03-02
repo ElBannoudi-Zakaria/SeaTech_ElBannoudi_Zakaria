f3cd5c44c2ff3bfb9e42768c6b5f8a5d4c69f92c
Robot <touloniut@gmail.com>
2023-03-02 12:57:54 +0100

    makhdemsh

--- C#/WpfApp1/MainWindow.xaml.cs
+++ C#/WpfApp1/MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace WpfApp1
     {
         ReliableSerialPort serialPort1;
         DispatcherTimer timerAffichage;
-        
+
         Robot robot = new Robot();
         //string robot.receivedText;
 
@@ -36,7 +36,7 @@ namespace WpfApp1
             serialPort1 = new ReliableSerialPort("COM8", 115200, Parity.None, 8, StopBits.One);
             serialPort1.DataReceived += SerialPort1_DataReceived;
             serialPort1.Open();
-            
+
             timerAffichage = new DispatcherTimer();
             timerAffichage.Interval = new TimeSpan(0, 0, 0, 0, 100);
             timerAffichage.Tick += TimerAffichage_Tick;
@@ -45,18 +45,16 @@ namespace WpfApp1
 
         private void TimerAffichage_Tick(object sender, EventArgs e)
         {
-            while(robot.byteListReceived.Count>0)
+            while (robot.byteListReceived.Count > 0)
             {
                 byte c = robot.byteListReceived.Dequeue();
-                textBoxReception.Text += "0x" + c.ToString("X2") + " ";
-                //textBoxReception.Text += c.ToString("X");
-                //textBoxReception.Text += c.ToString("X2");
-                //textBoxReception.Text += c.ToString("X4");
+                DecodeMessage(c);
+                //textBoxReception.Text += "0x" + c.ToString("X2") + " ";
             }
         }
 
-        
-        
+
+
         private void SerialPort1_DataReceived(object sender, DataReceivedArgs e)
         {
             robot.receivedText += Encoding.UTF8.GetString(e.Data, 0, e.Data.Length);
@@ -64,7 +62,7 @@ namespace WpfApp1
             {
                 robot.byteListReceived.Enqueue(e.Data[i]);
             }
-            
+
         }
 
         private void buttonEnvoyer_Click(object sender, RoutedEventArgs e)
@@ -81,7 +79,7 @@ namespace WpfApp1
         {
             if (e.Key == Key.Enter)
             {
-                buttonEnvoyer_Click(sender,e);
+                buttonEnvoyer_Click(sender, e);
 
             }
 
@@ -94,13 +92,121 @@ namespace WpfApp1
         }
 
         byte[] byteList = new byte[20];
+
         private void buttonTest_Click(object sender, RoutedEventArgs e)
-        {            
-            for (int i = 0; i < 20; i++)
+        {
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    byteList[i] = (byte)(2 * i);
+            //}
+            //serialPort1.Write(byteList, 0, byteList.Length);
+            byte[] array = Encoding.ASCII.GetBytes("Bonjour");
+            UartEncodeAndSendMessage(0x1234, array.Length, array);
+        }
+
+        byte CalculateChecksum(int msgFunction, int msgPayloadLength, byte[] msgPayload)
+        {
+            byte checksum = 0;
+            checksum ^= 0xFE;
+            checksum ^= (byte)(msgFunction >> 8);
+            checksum ^= (byte)(msgFunction >> 0);
+            checksum ^= (byte)(msgPayloadLength >> 8);
+            checksum ^= (byte)(msgPayloadLength >> 0);
+            for (int i = 0; i < msgPayloadLength; i++)
             {
-                byteList[i] = (byte)(2 * i);
+                checksum ^= msgPayload[i];
             }
-            serialPort1.Write(byteList, 0, byteList.Length);
+            return checksum;
+        }
+
+        void UartEncodeAndSendMessage(int msgFunction, int msgPayloadLength, byte[] msgPayload)
+        {
+            byte[] msg = new byte[6 + msgPayloadLength];
+            msg[0] = 0xFE;
+            msg[1] = (byte)(msgFunction >> 8);
+            msg[2] = (byte)(msgFunction >> 0);
+            msg[3] = (byte)(msgPayloadLength >> 8);
+            msg[4] = (byte)(msgPayloadLength >> 0);
+            for (int i = 0; i < msgPayloadLength; i++)
+            {
+                msg[5 + i] = msgPayload[i];
+            }
+            msg[5 + msgPayloadLength] = CalculateChecksum(msgFunction, msgPayloadLength, msgPayload);
+            serialPort1.Write(msg, 0, msg.Length);
+        }
+
+
+        public enum StateReception
+        {
+            Waiting,
+            FunctionMSB,
+            FunctionLSB,
+            PayloadLengthMSB,
+            PayloadLengthLSB,
+            Payload,
+            CheckSum
+        }
+
+        StateReception rcvState = StateReception.Waiting;
+        int msgDecodedFunction = 0;
+        int msgDecodedPayloadLength = 0;
+        byte[] msgDecodedPayload;
+        int msgDecodedPayloadIndex = 0;
+        private void DecodeMessage(byte c)
+        {
+
+            switch (rcvState)
+            {
+                case StateReception.Waiting:
+                    if (c == 0xFE)
+                    {
+                        rcvState = StateReception.FunctionMSB;
+                    }
+                    break;
+                case StateReception.FunctionMSB:
+                    msgDecodedFunction = c << 8;
+                    rcvState = StateReception.FunctionLSB;
+                    break;
+
+                case StateReception.FunctionLSB:
+                    msgDecodedFunction += c << 0;
+                    rcvState = StateReception.PayloadLengthMSB;
+                    break;
+                case StateReception.PayloadLengthMSB:
+                    msgDecodedPayloadLength = c << 8;
+                    rcvState = StateReception.PayloadLengthLSB;
+                    break;
+                case StateReception.PayloadLengthLSB:
+                    msgDecodedPayloadLength += c << 0;
+                    rcvState = StateReception.Payload;
+                    break;
+                case StateReception.Payload:
+                    if (msgDecodedPayloadIndex < msgDecodedPayloadLength)
+                    {
+                        msgDecodedPayload[msgDecodedPayloadIndex] = c;
+                        msgDecodedPayloadIndex += 1;
+                        rcvState = StateReception.Payload;
+
+                    }
+                    else
+                    {
+                        rcvState = StateReception.CheckSum;
+                    }
+                    break;
+
+                case StateReception.CheckSum:
+                    int calculatedChecksumvaleur = CalculateChecksum(msgDecodedFunction, msgDecodedPayloadLength, msgDecodedPayload);
+                    int receivedChecksum = c;
+                    if (calculatedChecksumvaleur == receivedChecksum)
+                    {
+                        textBoxReception.Text += "Message bien reçu";
+                    }
+                    textBoxReception.Text += "Message corrompu";
+                    break;
+                default:
+                    rcvState = StateReception.Waiting;
+                    break;
+            }
         }
     }
 }
